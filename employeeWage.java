8c7a1d05cfe217c6e52a2d54e63df1906a790bd9
unknown <mfjkd7795@gmail.com>
2021-06-21 12:58:20 -0700

    Employee 20 days wage

--- employeeWage.java
+++ employeeWage.java
@@ -2,6 +2,31 @@ package EmployeeJava;
 
 public class employeeWage {
 public static void main(String[] args) {
-	System.out.println("Welcom to Employee Wage Computation Program");
+	int PartTimeHours = 4;
+	int FullDay = 8;
+	int WarkingHourDay = 100;
+	int WorkingDayParMonth = 20;
+	final int empPresent = 1;
+	final int empPartTime = 2;
+	int dailyWage = 0;
+
+	int empCheck = (int)Math.floor(Math.random() * 10) % 3;
+	
+	switch (empCheck) {
+	case empPresent:
+		dailyWage = FullDay*WarkingHourDay*WorkingDayParMonth;
+		System.out.println("Employee is Present");
+		System.out.println("Employee Daily 20 Day Wage Is : "+ dailyWage);
+		break;
+	case empPartTime:{
+		dailyWage = PartTimeHours*WarkingHourDay*WorkingDayParMonth;
+		System.out.println("Employee Part Time is Present");
+		System.out.println("Employee Part Time 20 Day Wage Is : "+ dailyWage);
+		break;
+	}
+	default:
+		System.out.println("Employee is Absent");
+		break;
+	}
 }
 }
