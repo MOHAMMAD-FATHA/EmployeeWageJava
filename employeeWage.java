c42918028e1103a859b37cd0494c2a229a445d73
unknown <mfjkd7795@gmail.com>
2021-06-21 12:30:42 -0700

    Employe Present or Absent

--- employeeWage.java
+++ employeeWage.java
@@ -2,6 +2,11 @@ package EmployeeJava;
 
 public class employeeWage {
 public static void main(String[] args) {
-	System.out.println("Welcom to Employee Wage Computation Program");
+	int empCheck = (int)Math.floor(Math.random() * 10) % 2;
+	if (empCheck == 1) {
+		System.out.println("Employee is Present");
+	}else {
+	System.out.println("Employee is Absent");
+	}
 }
 }
