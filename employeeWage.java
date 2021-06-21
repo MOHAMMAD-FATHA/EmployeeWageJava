8fae003fc3671748b899b7272a696e102f683fd5
unknown <mfjkd7795@gmail.com>
2021-06-21 12:39:22 -0700

    Employe daily wages

--- employeeWage.java
+++ employeeWage.java
@@ -2,6 +2,16 @@ package EmployeeJava;
 
 public class employeeWage {
 public static void main(String[] args) {
-	System.out.println("Welcom to Employee Wage Computation Program");
+	int FullDay = 8;
+	int WageParHour = 20;
+	int empPresent = 1;
+	int empCheck = (int)Math.floor(Math.random() * 10) % 2;
+	if (empCheck == empPresent) {
+		int dailyWage = FullDay*WageParHour;
+		System.out.println("Employee is Present");
+		System.out.println("Employee Daily Wage Is : "+ dailyWage);
+	}else {
+	System.out.println("Employee is Absent");
+	}
 }
 }
