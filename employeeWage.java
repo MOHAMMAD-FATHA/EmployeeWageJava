23e61a1a1d6df20ddba3cce531218b11ab68a4dd
unknown <mfjkd7795@gmail.com>
2021-06-21 12:42:17 -0700

    Employee part time and full time wage

--- employeeWage.java
+++ employeeWage.java
@@ -2,6 +2,24 @@ package EmployeeJava;
 
 public class employeeWage {
 public static void main(String[] args) {
-	System.out.println("Welcom to Employee Wage Computation Program");
+	int PartTimeHours = 4;
+	int FullDay = 8;
+	int WageParHour = 20;
+	int empPresent = 1;
+	int empPartTime = 2;
+	int empCheck = (int)Math.floor(Math.random() * 10) % 3;
+	if (empCheck == empPresent) {
+		int dailyWage = FullDay*WageParHour;
+		System.out.println("Employee is Present");
+		System.out.println("Employee Daily Wage Is : "+ dailyWage);
+	}
+	else if(empCheck == empPartTime){
+		int dailyWage = PartTimeHours*WageParHour;
+		System.out.println("Employee Part Time is Present");
+		System.out.println("Employee Part Time Wage Is : "+ dailyWage);
+	}
+	else {
+	System.out.println("Employee is Absent");
+	}
 }
 }
