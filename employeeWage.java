ee4356ff1e7fa4837c8d58dbeaf8415ce487c4c6
unknown <mfjkd7795@gmail.com>
2021-06-21 12:49:42 -0700

    Employee daily Wage using case

--- employeeWage.java
+++ employeeWage.java
@@ -2,6 +2,30 @@ package EmployeeJava;
 
 public class employeeWage {
 public static void main(String[] args) {
-	System.out.println("Welcom to Employee Wage Computation Program");
+	int PartTimeHours = 4;
+	int FullDay = 8;
+	int WageParHour = 20;
+	final int empPresent = 1;
+	final int empPartTime = 2;
+	int dailyWage = 0;
+
+	int empCheck = (int)Math.floor(Math.random() * 10) % 3;
+	
+	switch (empCheck) {
+	case empPresent:
+		dailyWage = FullDay*WageParHour;
+		System.out.println("Employee is Present");
+		System.out.println("Employee Daily Wage Is : "+ dailyWage);
+		break;
+	case empPartTime:{
+		dailyWage = PartTimeHours*WageParHour;
+		System.out.println("Employee Part Time is Present");
+		System.out.println("Employee Part Time Wage Is : "+ dailyWage);
+		break;
+	}
+	default:
+		System.out.println("Employee is Absent");
+		break;
+	}
 }
 }
